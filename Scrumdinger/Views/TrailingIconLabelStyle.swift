8cffe9a2e79d9825b787dda0c6691e75028d5693
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-04 23:31:18 +0100

    Add title and icon configurations

--- Scrumdinger/Views/TrailingIconLabelStyle.swift
+++ Scrumdinger/Views/TrailingIconLabelStyle.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct TrailingIconLabelStyle: LabelStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
-            
+            configuration.title
+            configuration.icon
         }
     }
 }
