28d6bec935d17307d9de2fe10947d8f7eac06294
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-04 22:15:20 +0100

    Update text view to show title of scrum

--- Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Views/CardView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(scrum.title)
+            .font(.headline)
     }
 }
 
