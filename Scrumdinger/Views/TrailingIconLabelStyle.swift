2d0759ab565a43289979112ea9ca7c7ed43846f6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-05 12:43:08 +0100

    Part 2 - Creating a card view  (#2)
    * Build a CardView that displays a summary of a daily scrum

---
+++ Scrumdinger/Views/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 04/04/2024.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
