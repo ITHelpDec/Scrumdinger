4682a764d5398e7074b9110257dfd8a8014363cc
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-08 00:15:27 +0100

    Add label for theme

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -22,6 +22,9 @@ struct DetailView: View {
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
                 .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                }
             }
         }
     }
