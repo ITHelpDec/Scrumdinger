4a038da58109cc6ab331c3787beef33be9785c77
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-07 20:34:11 +0100

    Part 3 - Display data in a list (#3)
    * Use a List view to display a summary of the user’s daily scrums

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 07/04/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColour)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
