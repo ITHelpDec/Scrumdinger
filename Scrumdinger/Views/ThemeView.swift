c8b7ad8c7740d094e494dddacc865dca9c98d781
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-09 19:50:14 +0100

    Create ThemeView SwiftUI file

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
