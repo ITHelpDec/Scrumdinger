85f8a3e1190547ffcf1c4252895b93e661f1e35d
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-09 22:11:35 +0100

    Add private state var in ScrumdingerApp

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+    
     var body: some Scene {
         WindowGroup {
             ScrumsView(scrums: DailyScrum.sampleData)
