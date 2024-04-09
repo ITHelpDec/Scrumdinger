c0113e9741b44ac32f8f7ed9b0f92ea68cf833b2
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-10 00:17:33 +0100

    Part 6 - Passing data with bindings (#6)
    * Expand the editing capabilities by building a colour theme picker using bindings that allow the theme picker and edit views to share their data

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(theme.mainColour)
+            .foregroundColor(theme.accentColour)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
