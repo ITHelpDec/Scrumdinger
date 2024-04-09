66d41285a4eda1060220f1a23a5421bc3230d3a1
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-09 18:56:01 +0100

    Add Done button

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -54,6 +54,11 @@ struct DetailView: View {
                                 isPresentingEditView = false
                             }
                         }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
                     }
             }
         }
