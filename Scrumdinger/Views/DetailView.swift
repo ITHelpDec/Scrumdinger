e9c4b432592c0f95cbb0ded780a730fb9c054463
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-09 21:05:52 +0100

    Add private State var for editing scrum

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     
+    @State private var editingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
     
     var body: some View {
