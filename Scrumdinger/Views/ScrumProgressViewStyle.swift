80fed0e6b68627fed7c8d0795bfb770866385177
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-10 20:43:56 +0100

    Create ScrumProgressViewStyle SwiftUI file

---
+++ Scrumdinger/Views/ScrumProgressViewStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 10/04/2024.
+//
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumProgressViewStyle()
+}
