5aff8b4851422e22ca3ace5beb56b435ebbc1891
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-10 20:42:00 +0100

    Change progress view style
      * file is included in Starting Project

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -26,6 +26,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
