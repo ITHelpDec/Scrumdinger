0e661cdccacd6bad53d03beafc08e358a677aae3
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-11 09:21:09 +0100

    Create and pass skipAction property

--- Scrumdinger/Views/MeetingFooterView.swift
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MeetingFooterView: View {
     let speakers: [ScrumTimer.Speaker]
+    var skipAction: ()->Void
     var body: some View {
         HStack {
             Text("Speaker 1 of 3")
@@ -22,6 +23,6 @@ struct MeetingFooterView: View {
 }
 
 #Preview {
-    MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers)
+    MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
         .previewLayout(.sizeThatFits)
 }
