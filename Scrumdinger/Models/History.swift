d6cbce864b5319df9987f30ffa13dba718f26db8
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-11 23:29:37 +0100

    Create initialiser for default values

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -11,4 +11,10 @@ struct History: Identifiable {
     let id: UUID
     let date: Date
     var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
 }
