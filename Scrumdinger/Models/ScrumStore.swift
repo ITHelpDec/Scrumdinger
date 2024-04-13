ebd1edd3b91669fa90dabaee3dce97f291459ee3
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-13 18:33:33 +0100

    Call url on the default file manager
      * you use the shared instance of the FileManager class to get the location of the Documents directory for the current user

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -11,6 +11,9 @@ class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
     
     private static func fileURL() throws -> URL {
-        
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
     }
 }
