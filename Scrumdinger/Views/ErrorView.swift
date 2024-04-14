4da7e1eda2cc6c8c1febf8aa352e15f18561496a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-14 15:52:15 +0100

    Embed existing text in padded VStack with Spacer

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -11,7 +11,11 @@ struct ErrorView: View {
     let errorWrapper: ErrorWrapper
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
+        .padding()
     }
 }
 
