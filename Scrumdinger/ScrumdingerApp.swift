84e94902a1da9b7a6afbecf147b7d7411b56c30d
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-14 10:46:05 +0100

    Use a do-catch to try save the store scrums

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -15,7 +15,11 @@ struct ScrumdingerApp: App {
         WindowGroup {
             ScrumsView(scrums: $store.scrums) {
                 Task {
-                    
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
                 }
             }
             .task {
