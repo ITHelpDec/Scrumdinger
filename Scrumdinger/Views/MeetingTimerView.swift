1f9d543d5ca7bf28db25cf9d0a9cadfbc21efe2c
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-16 00:24:15 +0100

    Create MeetingTimerView SwiftUI View file

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 16/04/2024.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingTimerView()
+}
