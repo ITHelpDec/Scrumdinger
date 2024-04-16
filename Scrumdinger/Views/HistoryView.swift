ff52a05e26332d2098d67d60ffca8775526bcf29
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-16 17:17:16 +0100

    Add sample history data to HistoryView Preview

--- Scrumdinger/Views/HistoryView.swift
+++ Scrumdinger/Views/HistoryView.swift
@@ -16,7 +16,17 @@ struct HistoryView: View {
 }
 
 struct HistoryViewPreview: PreviewProvider {
+    static var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis"),
+        ],
+                transcript: "Darla, would you like to start today? " +
+                            "Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    }
+    
     static var previews: some View {
-        HistoryView()
+        HistoryView(history: history)
     }
 }
