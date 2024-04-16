c333b16c8fca47eab90b2dfded9a4af4f8b323cf
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-16 17:06:38 +0100

    Create HistoryView SwiftUI with history const

---
+++ Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by ITHelpDec on 16/04/2024.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
