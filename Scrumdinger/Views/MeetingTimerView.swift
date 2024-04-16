c9c14b0b9f291c3c302cae6f87ea6b75f5c71112
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-16 09:05:44 +0100

    Declare speakers and theme to pass to preview

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
     var body: some View {
         Circle()
             .strokeBorder(lineWidth: 24)
@@ -18,7 +21,11 @@ struct MeetingTimerView: View {
 }
 
 struct MeetingTimerView_Preview: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true),
+         ScrumTimer.Speaker(name:"Cathy", isCompleted: false)]
+    }
     static var previews: some View {
-        MeetingTimerView()
+        MeetingTimerView(speakers: speakers, theme: .yellow)
     }
 }
