d123e2acda01d774ab8a9b16253b6265a130384f
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-04-16 17:23:07 +0100

    Embed aligned VStack inside of ScrollView

--- Scrumdinger/Views/HistoryView.swift
+++ Scrumdinger/Views/HistoryView.swift
@@ -11,7 +11,11 @@ struct HistoryView: View {
     let history: History
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                
+            }
+        }
     }
 }
 
